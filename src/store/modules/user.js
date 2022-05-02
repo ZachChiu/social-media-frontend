2e3a65f384fa2a40b4ad906f3fb09387de9dcfa3
Chiu Zach <awdx8326@yahoo.com.tw>
2022-05-02 13:53:54 +0800

    init

---
+++ src/store/modules/user.js
@@ -0,0 +1,14 @@
+const state = {
+  de: "1",
+};
+const getters = {};
+const mutations = {};
+const actions = {};
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  mutations,
+  actions,
+};
